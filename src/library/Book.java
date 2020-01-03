f3d5145eddc7435db1d2608cb8f3812d90f4cd1b
Mikołaj Jankowski <mikolajankowski@gmail.com>
2020-01-03 22:23:00 +0100

    Initial commit

---
+++ src/library/Book.java
@@ -0,0 +1,10 @@
+package library;
+
+public class Book {
+    String title;
+    String author;
+    int releaseDate;
+    int pages;
+    String publishisher;
+    String isbn;
+}
---
+++ src/library/Library.java
@@ -0,0 +1,25 @@
+package library;
+
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.2";
+
+        Book book1 = new Book();
+
+        book1.title = "W pustyni i w puszyczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releaseDate = 2010;
+        book1.pages = 296;
+        book1.publishisher = "Greg";
+        book1.isbn = "978837371890";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publishisher);
+        System.out.println(book1.isbn);
+    }
+}
