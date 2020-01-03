3c04ebc9c238824bdbe864d299422317bcce2987
Mikołaj Jankowski <mikolajankowski@gmail.com>
2020-01-03 23:08:12 +0100

    ZIOM

--- src/library/Library.java
+++ src/library/Library.java
@@ -21,5 +21,6 @@ public class Library {
         System.out.println(book1.pages);
         System.out.println(book1.publishisher);
         System.out.println(book1.isbn);
+        System.out.println("ZIOM");
     }
 }
