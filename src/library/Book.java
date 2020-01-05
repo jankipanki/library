8322443c7fec7056a057bde5ba6b901de681173a
mikolajankowski@gmail.com <Play7965>
2020-01-06 00:35:21 +0100

    Wawer 06.01.2020

--- src/library/Book.java
+++ src/library/Book.java
@@ -7,4 +7,18 @@ public class Book {
     int pages;
     String publishisher;
     String isbn;
+
+    public Book(String title, String author, int releaseDate, int pages, String publishisher, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publishisher = publishisher;
+        this.isbn = isbn;
+    }
+
+    void printInfo() {
+        String info = this.title + "; " + this.author + "; " + this.releaseDate + "; " + this.pages + "; " + this.publishisher + "; " + this.isbn;
+        System.out.println(info);
+    }
 }
